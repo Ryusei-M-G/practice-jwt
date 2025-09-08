bd8ad271cf68ff375e69e3e24c4eb97c48654f72
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-09-08 14:31:09 +0900

    Add jwtAuth

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 const port = 3000
 
+const payload = { userId: 1}
+const JWT_SECRET = 'secret-key'
+
+const auth = (req, res) => {
+  const token = jwt.sign(
+    payload,
+    JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+  res.json({ token });
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({message:'hello world'});
 })
 
+app.get('/auth',auth);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
+
